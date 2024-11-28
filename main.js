de8c999cc6d116aa3c7a7a0a15a7e33c7961e377
Eduardo Lucio de Oliviera <eduardo.oliveiira2005@gmail.com>
2024-11-28 13:04:21 -0300

    Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const menuNav = document.querySelector('.header_span--input');
+const menuNavbar = document.querySelector('.menu-bar--check');
+
+menuNav.addEventListener('click', () => {
+    if (menuNav.checked == true){
+        menuNavbar.style.left = '0';
+    } 
+});
+
+document.addEventListener('click', (event) => { 
+    if (!menuNavbar.contains(event.target) && event.target !== menuNav) { 
+        menuNavbar.style.left = '-100%'; 
+    } 
+});
+
+
+const buttonFilter = document.querySelector('#checked');
+const filter = document.querySelector('.menu-filtered');
+const ico = document.querySelector('#iconFilter');
+
+buttonFilter.addEventListener ('click',() => {
+    if(buttonFilter.checked == true){
+        filter.style.top = '24%'
+        setTimeout(() => {
+            ico.classList.remove('fa-chevron-down');
+            ico.classList.add('fa-chevron-up');
+            console.log('final')
+        }, 350);
+    } else {
+        filter.style.top = '-100%'
+        ico.classList.add('fa-chevron-down');
+        ico.classList.remove('fa-chevron-up');
+        console.log('inicio')
+    }
+})
